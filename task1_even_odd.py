6e9a8d0daa07b973edf4eeb0bc893dad815e3f24
Somil Thakker <thakkersomil@gmail.com>
2025-07-22 12:43:29 +0530

    Assigment 2 , task1_even_odd.py task2_sum_loop.py

---
+++ task1_even_odd.py
@@ -0,0 +1,7 @@
+#task1_even_odd
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+	print(f"{num} is an even number.")
+else:
+	print(f"{num} is not an even number.")
---
+++ task2_sum_loop.py
@@ -0,0 +1,7 @@
+#task2_sum_loop
+count = 0
+
+for i in range (1,51):
+	count = count + i
+print(f"The sum of numbers from 1 to 50 is:, {count}")
+
